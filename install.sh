b639d01b28096cc0fefafa249b24ac7a10618f61
Jack Jennings <jack@standard-library.com>
2018-10-12 13:28:21 -0700

    Fix issues in installation script

--- install.sh
+++ install.sh
@@ -7,17 +7,20 @@ fi
 
 DEFAULT_URL=media.staging.sistercitynyc.com
 
+echo "Updating..."
 apt-get update
+
+echo "Installing ffmpeg..."
 apt-get -y install ffmpeg
 
 echo "Creating configuration file..."
-if [ ! -f ~/.skycamrc ]; then
-  read -e -p "Enter URL [media.staging.sistercitynyc.com]:\n> " url
-  url=${url:-"media.staging.sistercitynyc.com"}
+if [ ! -f /home/pi/.skycamrc ]; then
+  read -e -p "> Enter URL [media.staging.sistercitynyc.com]: " url
+  url=${url:-DEFAULT_URL}
 
-  read -e -p "Enter UUID:\n> " id
+  read -e -p "> Enter stream UUID: " id
 
-  cat >~/.skcamrc <<EOL
+  cat >/home/pi/.skycamrc <<EOL
 STREAM_URL=${url}
 STREAM_ID=${id}
 EOL
@@ -25,18 +28,20 @@ fi
 
 echo "Linking broadcast script..."
 if [ ! -f /usr/local/bin/broadcast ]; then
-  ln -s ./broadcast /usr/local/bin/broadcast
+  ln -s $(pwd)/broadcast /usr/local/bin/broadcast
 fi
 
 echo "Linking broadcast service..."
 if [ ! -f /lib/systemd/system/broadcast.service ]; then
-  ln -s ./broadcast.service /lib/systemd/system/broadcast.service
+  ln -s $(pwd)/broadcast.service /lib/systemd/system/broadcast.service
 fi
 
 echo "Enabling broadcast service..."
 systemctl enable broadcast
 
 echo "The last step is to enable the camera module.\n"
-echo "raspi-config will now launch. A reboot will be required\n"
-echo "to finish the installation."
+echo "raspi-config will now launch, and you will need to\n"
+echo "follow the prompts to enable the camera hardware\n"
+echo "A reboot will be required to finish the installation."
 read -rsp $'> Press any key to continue...\n' -n1 key
+raspi-config
